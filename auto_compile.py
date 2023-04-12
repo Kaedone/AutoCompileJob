aeb32d54a8d3a1b802cde7c7836e384a27423351
Kaedone <timstab1@gmail.com>
2023-04-12 23:43:30 +0300

    Added read variables from the environment

--- auto_compile.py
+++ auto_compile.py
@@ -1,31 +1,40 @@
 import os
 import subprocess
-import time
 from git import Repo
 from PyPDF2 import PdfFileMerger, PdfFileReader
 
+
+# функция для извлечения переменных из enviroment
+def get_env_var(name):
+    if name in os.environ:
+        return os.environ[name]
+    else:
+        raise Exception("{} not found in environment variables".format(name))
+
+
 # Путь к локальному репозиторию
-REPO_PATH = "/path/to/repo"
+REPO_PATH = get_env_var("REPO_PATH")
 
 # Путь к папке с исходными файлами
-SOURCE_PATH = "/path/to/repo/source"
+SOURCE_PATH = get_env_var("SOURCE_PATH")
 
 # Путь к папке с скомпилированными файлами
-OUTPUT_PATH = "/path/to/repo/output"
+OUTPUT_PATH = get_env_var("OUTPUT_PATH")
 
 # Название ветки, за изменениями которой следим
-BRANCH_NAME = "master"
+BRANCH_NAME = get_env_var("BRANCH_NAME")
 
 # Команда для компиляции исходных файлов
 COMPILE_COMMAND = "pdflatex -output-directory={} {}"
 
+
 # Функция для компиляции файла в формат PDF
-def compile_to_pdf(filename):
+def compile_to_pdf(file_name):
     # Имя файла без расширения
-    basename = os.path.splitext(filename)[0]
+    basename = os.path.splitext(file_name)[0]
 
     # Путь к исходному файлу
-    source_file = os.path.join(SOURCE_PATH, filename)
+    source_file = os.path.join(SOURCE_PATH, file_name)
 
     # Путь к выходному файлу
     output_file = os.path.join(OUTPUT_PATH, basename + ".pdf")
@@ -36,24 +45,24 @@ def compile_to_pdf(filename):
     # Запускаем процесс компиляции
     subprocess.run(command, shell=True, cwd=SOURCE_PATH)
 
-    
     return output_file
+
 
 # Функция для объединения нескольких PDF-файлов в один
 def merge_pdfs(filenames, output_file):
     merger = PdfFileMerger()
-    for filename in filenames:
-        with open(filename, "rb") as f:
+    for file_name in filenames:
+        with open(file_name, "rb") as f:
             merger.append(PdfFileReader(f))
     with open(output_file, "wb") as f:
         merger.write(f)
+
 
 # Инициализируем репозиторий
 repo = Repo(REPO_PATH)
 
 # Получаем текущую ветку
 branch = repo.head.reference
-
 
 while True:
     # Проверяем изменения в репозитории
@@ -70,15 +79,14 @@ while True:
                 pdf_file = compile_to_pdf(filename)
                 pdf_files.append(pdf_file)
 
-        # Объединяем PDF-файлы в один
+            # Объединяем PDF-файлы в один
             if pdf_files:
                 output_file = os.path.join(OUTPUT_PATH, "output.pdf")
                 merge_pdfs(pdf_files, output_file)
                 print("Compilation finished, output file: {}".format(output_file))
 
-    # Обновляем указатель на последний коммит
+            # Обновляем указатель на последний коммит
             branch.commit = repo.head.commit
 
 # Ждем некоторое время перед следующей проверкой изменений
 time.sleep(60)
-
